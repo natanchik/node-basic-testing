5065037fdae79b21b6819094b235bb7f9581e46b
Natalia Lebedeva <nataliadaveng@gmail.com>
2024-10-12 19:14:21 +0500

    revert: add Simple tests
    This reverts commit ace730652778cdec14e608e7242c6b3eda0a8941.

--- src/01-simple-tests/index.test.ts
+++ src/01-simple-tests/index.test.ts
@@ -1,35 +1,32 @@
-import { simpleCalculator, Action } from './index';
+// Uncomment the code below and write your tests
+// import { simpleCalculator, Action } from './index';
 
 describe('simpleCalculator tests', () => {
   test('should add two numbers', () => {
-    expect(simpleCalculator({ a: 1, b: 2, action: Action.Add })).toBe(3);
+    // Write your test here
   });
 
   test('should subtract two numbers', () => {
-    expect(simpleCalculator({ a: 5, b: 3, action: Action.Subtract })).toBe(2);
+    // Write your test here
   });
 
   test('should multiply two numbers', () => {
-    expect(simpleCalculator({ a: 2, b: 3, action: Action.Multiply })).toBe(6);
+    // Write your test here
   });
 
   test('should divide two numbers', () => {
-    expect(simpleCalculator({ a: 16, b: 4, action: Action.Divide })).toBe(4);
+    // Write your test here
   });
 
   test('should exponentiate two numbers', () => {
-    expect(simpleCalculator({ a: 5, b: 2, action: Action.Exponentiate })).toBe(
-      25,
-    );
+    // Write your test here
   });
 
   test('should return null for invalid action', () => {
-    expect(simpleCalculator({ a: 5, b: 2, action: 'string' })).toBeNull();
+    // Write your test here
   });
 
   test('should return null for invalid arguments', () => {
-    expect(
-      simpleCalculator({ a: 'string', b: 'string', action: Action.Add }),
-    ).toBeNull();
+    // Write your test here
   });
 });
